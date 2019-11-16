a2987d8b2a165cc73c5c86c76b7fe83731b7f5e7
Nuttapon <nhoon00slow@gmail.com>
2019-11-16 19:02:33 +0700

    add

--- app/src/main/java/com/example/restaurant/MainActivity.java
+++ app/src/main/java/com/example/restaurant/MainActivity.java
@@ -4,14 +4,18 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.os.Handler;
+import android.widget.ListView;
+
+import static com.example.restaurant.R.id.listveiw;
 
 public class MainActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
     }
-
-
 }
--- app/src/main/java/com/example/restaurant/confirmActivity.java
+++ app/src/main/java/com/example/restaurant/confirmActivity.java
@@ -3,12 +3,23 @@ package com.example.restaurant;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 public class confirmActivity extends AppCompatActivity {
 
+    ListView listView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_confirm);
+
+        listView = (ListView) findViewById(R.id.listveiw);
+
+        ArrayList<String> arrayList = new ArrayList<>();
+
+        arrayList.add("sadsa");
+        arrayList.add("3131");
     }
 }
